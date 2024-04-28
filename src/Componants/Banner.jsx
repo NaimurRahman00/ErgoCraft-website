aedb5f31570432223559bb3aac4ab7e62c930996
Naimur00 <aminaim4u@gmail.com>
2024-04-28 05:34:30 -0600

    Banner updated

--- src/Componants/Banner.jsx
+++ src/Componants/Banner.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
+import { Typewriter } from "react-simple-typewriter";
 
 const Banner = () => {
   const [currentSlider, setCurrentSlider] = useState(0);
-  
+
   const sliders = [
     "https://i.ibb.co/svwWHhs/jute-bag.jpg",
-    "https://i.ibb.co/hyFHfk4/wood-spoon.jpg",
-    "https://i.ibb.co/3mFhyQD/wood-plate.jpg"
+    "https://i.ibb.co/3mFhyQD/wood-plate.jpg",
+    "https://i.ibb.co/phVf6Vx/2151003070.jpg",
   ];
   const nextSlider = () =>
     setCurrentSlider((currentSlider) =>
@@ -22,11 +23,21 @@ const Banner = () => {
   return (
     <div>
       <div className="w-full mx-auto h-[240px] md:h-[540px] flex flex-col lg:flex-row items-center overflow-hidden gap-5 lg:gap-10 mt-10 relative ">
-        <div className="absolute z-50 bg-gradient-to-r flex flex-col items-start from-black/80 top-0 left-0 right-0 bottom-0 p-20 py-24 rounded-b-2xl">
+        <div className="absolute z-50 bg-gradient-to-tr flex flex-col items-start from-black/95 via-black/40 top-0 left-0 right-0 bottom-0 p-20 py-24 rounded-b-2xl">
           <h2 className="text-white text-lg mb-2">Jute & Wood craft</h2>
-          <h1 className="font-unbounded text-[4.7rem] leading-[5.5rem] text-white font-bold">Raining offers <br /> for hot summer</h1>
+          <h1 className="font-unbounded text-[4.7rem] leading-[5.5rem] text-white font-bold">
+            Raining offers <br />for hot {" "}
+            <Typewriter
+              words={["summer", "Season"]}
+              loop={500}
+              typeSpeed={100}
+              deleteSpeed={100}
+            />
+          </h1>
           <p className="text-white text-2xl mt-4">25% off on all products</p>
-          <button className="btn rounded-full px-10 font-bold text-base mt-5 border-yellow-500 hover:bg-transparent hover:text-white hover:border-yellow-500 border-2 bg-yellow-500 ">Shop Now</button>
+          <button className="btn rounded-full px-10 font-bold text-base mt-5 border-yellow-500 hover:bg-transparent hover:text-white hover:border-yellow-500 border-2 bg-yellow-500 ">
+            Shop Now
+          </button>
         </div>
         <div className="relative overflow-hidden rounded-b-2xl w-full">
           {/* dots */}
